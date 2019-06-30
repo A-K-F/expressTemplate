90222f7bb6b629fd457c80ea8a16a1bc22e9a9da
Adam K. Foote <>
2019-06-30 14:54:05 -0700

    Added a default check for NODE_ENV

--- envValidate.js
+++ envValidate.js
@@ -4,4 +4,6 @@ const assert = require('assert');
 
 console.log('Checking ENV Vars');
 
+assert(['development', 'production'].includes(process.env.NODE_ENV), `NODE_ENV must be set to 'development' or 'production'`)
+
 console.log('ENV Vars pass basic validation');
